398ec8e9b7548659a4e583b1e4887af5b1361903
NitinShekapure003 <NitinShekapure003@gmail.com>
2023-04-28 16:22:49 +0530

    Updated comment in add_two.py

--- add_two.py
+++ add_two.py
@@ -1,4 +1,4 @@
-a= int(float(input ("Enter Num a : "))) # Getting first input from the user
-b= int(float(input ("Enter Num b : "))) # Getting second input from the user
-print (a+b)  # sum of the provided input
+a= int(float(input ("Enter Num a : ")))  # Getting first input from the user
+b= int(float(input ("Enter Num b : ")))  # Getting second input from the user
+print (a+b)  # sum of the given two input
 
