dc637bf17928c27b66529ab545dc8d182a061cfc
NitinShekapure003 <NitinShekapure003@gmail.com>
2023-04-28 18:38:52 +0530

    For Obseravtion add_two.py

--- add_two.py
+++ add_two.py
@@ -2,4 +2,4 @@ a= int(float(input ("Enter Num a : ")))  # Getting first input from the user
 b= int(float(input ("Enter Num b : ")))  # Getting second input from the user
 print (a+b)  # sum of the given two input
 
-# Experimental
+  # for observation purpose
